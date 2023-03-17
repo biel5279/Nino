d3f4a71ad5721022db478847fecd2255a17a3880
Laboratorio-Info <gabrielaraujo0704@gmail.com>
2023-03-16 22:14:48 -0300

    bahdbashkd

---
+++ app/src/main/java/br/com/etecia/nino/NinoLogin.java
@@ -0,0 +1,14 @@
+package br.com.etecia.nino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NinoLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nino_login);
+    }
+}
